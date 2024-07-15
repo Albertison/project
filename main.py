3fa5fd4ff84a24af77674d6c896403786c8ead4c
Albertison <xazivalieva2@gmail.com>
2024-07-15 12:19:51 +0300

    17

--- main.py
+++ main.py
@@ -20,9 +20,14 @@ def pririode_taiasha_v_sebe():
 
 @app.route('/pririode_taiasha_v_sebe_glava_1_spisok_romid')
 def pririode_taiasha_v_sebe_glava_1_spisok_romid():
-    return render_template('pririode_taiasha_v_sebe_glava_1_spisok_romid.html', title='Глава 1')
+    return render_template('pririode_taiasha_v_sebe_glava_1_spisok_romid.html', title='Список')
 
 
 @app.route('/pririode_taiasha_v_sebe_glava_1_romid_1')
 def pririode_taiasha_v_sebe_glava_1_romid_1():
-    return render_template('pririode_taiasha_v_sebe_glava_1_romid_1.html', title='Глава 1')
+    return render_template('pririode_taiasha_v_sebe_glava_1_romid_1.html', title='Ромид 1')
+
+
+@app.route('/pririode_taiasha_v_sebe_glava_1_romid_2')
+def pririode_taiasha_v_sebe_glava_1_romid_2():
+    return render_template('pririode_taiasha_v_sebe_glava_1_romid_2.html', title='Ромид 1')
