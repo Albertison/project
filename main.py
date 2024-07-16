0cb7d592101604c9ed0b7416e09175ce4bdba538
Albertison <xazivalieva2@gmail.com>
2024-07-16 18:52:47 +0300

    20

--- main.py
+++ main.py
@@ -36,3 +36,28 @@ def pririode_taiasha_v_sebe_glava_1_romid_2():
 @app.route('/pririode_taiasha_v_sebe_glava_1_romid_3')
 def pririode_taiasha_v_sebe_glava_1_romid_3():
     return render_template('pririode_taiasha_v_sebe_glava_1_romid_3.html', title='Ромид 3')
+
+
+@app.route('/pririode_taiasha_v_sebe_glava_1_romid_4')
+def pririode_taiasha_v_sebe_glava_1_romid_4():
+    return render_template('pririode_taiasha_v_sebe_glava_1_romid_4.html', title='Ромид 4')
+
+
+@app.route('/pririode_taiasha_v_sebe_glava_1_romid_5')
+def pririode_taiasha_v_sebe_glava_1_romid_5():
+    return render_template('pririode_taiasha_v_sebe_glava_1_romid_5.html', title='Ромид 5')
+
+
+@app.route('/pririode_taiasha_v_sebe_glava_1_romid_6')
+def pririode_taiasha_v_sebe_glava_1_romid_6():
+    return render_template('pririode_taiasha_v_sebe_glava_1_romid_6.html', title='Ромид 6')
+
+
+@app.route('/pririode_taiasha_v_sebe_glava_1_romid_7')
+def pririode_taiasha_v_sebe_glava_1_romid_7():
+    return render_template('pririode_taiasha_v_sebe_glava_1_romid_7.html', title='Ромид 7')
+
+
+@app.route('/pririode_taiasha_v_sebe_glava_1_romid_8')
+def pririode_taiasha_v_sebe_glava_1_romid_8():
+    return render_template('pririode_taiasha_v_sebe_glava_1_romid_8.html', title='Ромид 8')
